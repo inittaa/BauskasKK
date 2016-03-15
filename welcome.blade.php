372cd07a2d6f66ae570ade3fdd94cabf1ec2653f
inittaa <inita.miglane@gmail.com>
2016-03-15 22:34:55 +0200

    First

---
+++ welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Dance</title>
+    <meta charset="UTF-8">
+        <link href="theme/favicon.ico" rel="icon" type="image/x-icon" />
+    <link rel="stylesheet" href="style.min.css" />                      
+    </head>
+    <body>
+        <div> <a href="#"> <img alt="Attēli" height="150" width="400" src="Photos/bauskas_kulturas_centrs_logo.jpg"/></a></div>
+        <div>
+            <nav>
+        <ul>
+        <li><a href="index.html">
+            <div> 
+                    <h2>Aktualitātes</h2>
+                    </div></a>
+        </li>
+                <li><a href="Vesture.html">
+            <div><h2>Mūsu kolektīvi</h2>
+                    </div></a>
+        </li>
+                <li><a href="Atteli.html">
+            <div><h2>Par mums</h2>            
+                    </div></a>
+                </li>
+                <li><a href="Video.html">
+            <div><h2>Kontakti</h2>         
+                    </div></a>
+                </li>
+                <li class="login"><a href="register.blade.php">
+                    <h2>Reģistrēties</h2>
+                    </a>
+                </li>
+                <li class="login"><a href="ienakt.blade.php">
+                    <h2>Ienākt</h2>
+                    </a>
+                </li>
+        </ul>
+            </nav>
+        </div> <br><br>
+        <br>
+            
+    <footer>
+        &copy; 2016. gada 25. maijs. Darbu veidoja: Inita Miglāne
+    </footer>
+    </body>
+</html>
+
+
