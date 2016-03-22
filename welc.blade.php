c5cbc59e79b0459d2d9bbdcf15800816e1472cc7
inittaa <inita.miglane@gmail.com>
2016-03-22 22:25:11 +0200

    Added files via upload
    New Welcome page

---
+++ welc.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="utf-8">
+    <title>BauskasKK</title>
+</head>
+<style type="text/css">
+    
+    body{
+        font-family: arial,verdana,tahoma;
+    }
+    #container {
+    margin: 40px auto;
+    }
+    h1 {
+    font: bold 65px/60px Helvetica, Arial, Sans-serif;
+    text-align: center; color: #eee;
+    text-shadow: 0px 2px 6px #333;
+    }
+    ul.gallery a {
+    float: left;
+    padding: 10px 10px 25px 10px;
+    background: #eee;
+    border: 1px solid #fff;
+    -moz-box-shadow: 0px 2px 15px #333;
+    position: relative;}
+    img {
+    max-width: 350px;
+    max-height: auto;
+    }
+    ul.gallery  a.pic-1, a.pic-4{
+    z-index: 1;
+    -webkit-transform: rotate(-10deg);
+    -moz-transform: rotate(-10deg);
+    }
+    ul.gallery li a.pic-2, a.pic-6 {
+        z-index: 5;
+        -webkit-transform: rotate(-3deg);
+        -moz-transform: rotate(-3deg);
+    }
+    ul.gallery  a.pic-3, a.pic-5 {
+        z-index: 3;
+        -webkit-transform: rotate(4deg);
+        -moz-transform: rotate(4deg);
+    }
+</style>
+<body>
+<div class="header">
+    <h1>Bauskas Kultūras centra pašdarbnieku kolektīvi</h1>
+</div>
+<br>
+<br>
+<div>
+    <ul class="gallery">   
+        <a href="#" class="pic-1">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>       
+        <a href="#" class="pic-2">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-3">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-1">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-5">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-6">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-3">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-4">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-5">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-6">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-3">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-4">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-5">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-6">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-3">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-4">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-5">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+        <a href="#" class="pic-6">
+          {{ Html::image('DSC_5346.JPG') }}
+        </a>
+    </ul>
+</div>
+</body>
+
+</html>
